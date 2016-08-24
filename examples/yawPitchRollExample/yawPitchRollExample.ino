3cbb1bb68bfe126b6e87e10b41f9366e9a905cc0
liming <liming@rokid.com>
2016-08-24 19:24:34 +0800

    first commit

---
+++ examples/yawPitchRollExample/yawPitchRollExample.ino
@@ -0,0 +1,38 @@
+#include <Wire.h>
+#include <FreeSixIMU.h>
+
+FreeSixIMU imu;
+
+
+void setup() {
+  
+  imu.init();
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  float angles[3];
+  float q[4];
+    
+  //imu.getYawPitchRoll(angles);  
+  //imu.getQ(q);
+  imu.getEuler(angles);
+
+  Serial.print(-angles[0]);   //yaw
+  Serial.print(",\t");
+  Serial.print(-angles[1]);  //pitch
+  Serial.print(",\t");
+  Serial.println(-angles[2]);  //roll
+
+
+  // Serial.print(q[0]);   
+  // Serial.print(",\t");
+  // Serial.print(q[1]);  
+  // Serial.print(",\t");
+  // Serial.print(q[2]); 
+  // Serial.print(",\t");
+  // Serial.println(q[3]);  
+
+}
